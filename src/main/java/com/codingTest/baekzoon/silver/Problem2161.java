0822a2df811f6dfb12f1f771ab18656809136314
KrongDev <leegeondev@gmail.com>
2024-10-12 22:42:53 +0900

    feat: 백준 실버 4 카드 1 문제 해결

---
+++ src/main/java/com/codingTest/baekzoon/silver/Problem2161.java
@@ -0,0 +1,27 @@
+package com.codingTest.baekzoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Problem2161 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            sb.append(queue.poll()).append(" ");
+            if (queue.isEmpty()) break;
+            queue.add(queue.poll());
+        }
+        System.out.println(sb);
+    }
+}

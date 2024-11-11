f133e1a5d78ea7fe4c4bbd8db468809302ead564
KrongDev <leegeondev@gmail.com>
2024-11-12 00:50:03 +0900

    feat: 백준 실버 1 완전 이진 트리 문제 해결

---
+++ src/main/java/com/codingTest/baekzoon/silver/Problem9932.java
@@ -0,0 +1,39 @@
+package com.codingTest.baekzoon.silver;
+
+import java.io.*;
+
+public class Problem9932 {
+
+    static StringBuilder[] sbs;
+    static String[] strr;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        strr = br.readLine().split(" ");
+        sbs = new StringBuilder[N];
+        for (int i = 0; i < N; i++) {
+            sbs[i] = new StringBuilder();
+        }
+
+        solve(0, strr.length - 1, 0);
+
+        for (int i = 0; i < N; i++) {
+            bw.write(sbs[i].toString() + "\n");
+        }
+
+
+        bw.flush();
+    }
+
+    public static void solve( int start, int end, int level) {
+        if(level == N) return;
+        int mid = (start + end) / 2;
+        sbs[level].append(strr[mid]).append(" ");
+
+        solve(start, mid - 1, level + 1);
+        solve(mid + 1, end, level + 1);
+    }
+}

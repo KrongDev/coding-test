ad4e46146bd450a1d54674aa51c3d6c79d2281e1
KrongDev <leegeondev@gmail.com>
2024-11-11 22:11:49 +0900

    feat: 백준 브론즈 2 모스 부호 문제 해결

---
+++ src/main/java/com/codingTest/baekzoon/bronze/Problem29701.java
@@ -0,0 +1,72 @@
+package com.codingTest.baekzoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Problem29701 {
+
+    public static Map<String, String> initMorseCodeMap() {
+        Map<String, String> morseCodeMap = new HashMap<>();
+        morseCodeMap.put(".-", "A");
+        morseCodeMap.put("-...", "B");
+        morseCodeMap.put("-.-.", "C");
+        morseCodeMap.put("-..", "D");
+        morseCodeMap.put(".", "E");
+        morseCodeMap.put("..-.", "F");
+        morseCodeMap.put("--.", "G");
+        morseCodeMap.put("....", "H");
+        morseCodeMap.put("..", "I");
+        morseCodeMap.put(".---", "J");
+        morseCodeMap.put("-.-", "K");
+        morseCodeMap.put(".-..", "L");
+        morseCodeMap.put("--", "M");
+        morseCodeMap.put("-.", "N");
+        morseCodeMap.put("---", "O");
+        morseCodeMap.put(".--.", "P");
+        morseCodeMap.put("--.-", "Q");
+        morseCodeMap.put(".-.", "R");
+        morseCodeMap.put("...", "S");
+        morseCodeMap.put("-", "T");
+        morseCodeMap.put("..-", "U");
+        morseCodeMap.put("...-", "V");
+        morseCodeMap.put(".--", "W");
+        morseCodeMap.put("-..-", "X");
+        morseCodeMap.put("-.--", "Y");
+        morseCodeMap.put("--..", "Z");
+        morseCodeMap.put(".----", "1");
+        morseCodeMap.put("..---", "2");
+        morseCodeMap.put("...--", "3");
+        morseCodeMap.put("....-", "4");
+        morseCodeMap.put(".....", "5");
+        morseCodeMap.put("-....", "6");
+        morseCodeMap.put("--...", "7");
+        morseCodeMap.put("---..", "8");
+        morseCodeMap.put("----.", "9");
+        morseCodeMap.put("-----", "0");
+        morseCodeMap.put("--..--", ",");
+        morseCodeMap.put(".-.-.-", ".");
+        morseCodeMap.put("..--..", "?");
+        morseCodeMap.put("---...", ":");
+        morseCodeMap.put("-....-", "-");
+        morseCodeMap.put(".--.-.", "@");
+        return morseCodeMap;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Map<String, String> morseCodeMap = initMorseCodeMap();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while(N-- > 0) {
+            String morseCode = st.nextToken();
+            sb.append(morseCodeMap.get(morseCode));
+        }
+        System.out.println(sb);
+    }
+}

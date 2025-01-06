9298e18e60c2d33837963a3ec91cdfb9a27b6636
Leegeon <leegeondev@gmail.com>
2025-01-07 07:45:57 +0900

    feat: 프로그래머스 Level1 옹알이(2) 해결

---
+++ src/main/java/com/codingTest/programers/level1/옹알이_2.java
@@ -0,0 +1,27 @@
+package com.codingTest.programers.level1;
+
+public class 옹알이_2 {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i = 0; i < babbling.length; i++) {
+            if(babbling[i].contains("ayaaya") || babbling[i].contains("yeye") || babbling[i].contains("woowoo") || babbling[i].contains("mama")) {
+                continue;
+            }
+
+            babbling[i] = babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("ye", " ");
+            babbling[i] = babbling[i].replace("woo", " ");
+            babbling[i] = babbling[i].replace("ma", " ");
+            babbling[i] = babbling[i].replace(" ", "");
+
+            if(babbling[i].length()  == 0) answer++;
+
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        String[] babbling = {"aya", "yee", "u", "maa"};
+        옹알이_2 res = new 옹알이_2();
+        System.out.println(res.solution(babbling));
+    }
+}

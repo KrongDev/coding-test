723f7fe358f0ddfc9591abc28934fe8e89ef8ae4
Leegeon <leegeondev@gmail.com>
2025-09-26 03:42:18 +0900

    feat: 백준 실버 1 쉬운 계단 수 문제 해결

---
+++ src/main/java/com/codingTest/baekzoon/silver/Problem10844.java
@@ -0,0 +1,35 @@
+package com.codingTest.baekzoon.silver;
+
+import java.io.*;
+
+public class Problem10844 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[N + 1][10];
+        long mod = 1000000000;
+
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        // 2. DP 테이블 채우기 (길이 2부터 N까지)
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i - 1][1] % mod;
+            dp[i][9] = dp[i - 1][8] % mod;
+
+            for (int j = 1; j <= 8; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+            }
+        }
+
+        long result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result = (result + dp[N][i]) % mod;
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}

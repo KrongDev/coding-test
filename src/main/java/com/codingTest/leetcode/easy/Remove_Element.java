74c29fa9eb10031fa467d66d089d9f3d852e57bd
Leegeon <leegeondev@gmail.com>
2025-11-21 03:21:58 +0900

    feat: LeetCode Remove_Element

---
+++ src/main/java/com/codingTest/leetcode/easy/Remove_Element.java
@@ -0,0 +1,13 @@
+package com.codingTest.leetcode.easy;
+
+public class Remove_Element {
+    public int removeElement(int[] nums, int val) {
+        int i = 0, len = nums.length;
+        for(int j = 0; j < len; j++) {
+            if(nums[j] != val) {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+}

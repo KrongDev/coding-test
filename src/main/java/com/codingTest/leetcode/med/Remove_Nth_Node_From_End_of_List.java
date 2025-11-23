19d43cb5bff668c3e8ebacbea9e01eb8dc69c27c
Leegeon <leegeondev@gmail.com>
2025-11-23 17:33:08 +0900

    feat: LeetCode Remove_Nth_Node_From_End_of_List

---
+++ src/main/java/com/codingTest/leetcode/med/Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,28 @@
+package com.codingTest.leetcode.med;
+
+public class Remove_Nth_Node_From_End_of_List {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+        for(int i = 0; i < n; i++){
+            fast = fast.next;
+        }
+        if(fast == null){
+            return head.next;
+        }
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}

7a7512e4098d3d2e34298a1d0e64a03aff0d3054
Leegeon <leegeondev@gmail.com>
2025-11-23 17:03:31 +0900

    feat: LeetCode _3SumClosest

---
+++ src/main/java/com/codingTest/leetcode/med/_3SumClosest.java
@@ -0,0 +1,34 @@
+package com.codingTest.leetcode.med;
+
+import java.util.Arrays;
+
+public class _3SumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int len = nums.length;
+
+        int res = nums[0] + nums[1] + nums[2];
+        if(len == 3) return res;
+        int l, r, sum;
+        for(int x = 0; x < len - 2; x++) {
+            if (x > 0 && nums[x] == nums[x - 1]) continue;
+
+            l = x + 1;
+            r = len - 1;
+            while(l < r) {
+                sum = nums[x] + nums[l] + nums[r];
+
+                if(Math.abs(sum - target) < Math.abs(res - target)) {
+                    res = sum;
+                }
+
+                if (sum < target) l++;
+                else if (sum > target) r--;
+                else return sum;
+            }
+        }
+
+        return res;
+    }
+
+}

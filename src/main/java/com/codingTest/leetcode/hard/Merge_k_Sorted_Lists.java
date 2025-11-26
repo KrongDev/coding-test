aad538d7a8339d275f127f82b6b693bba4056562
Leegeon <leegeondev@gmail.com>
2025-11-26 23:20:09 +0900

    feat: LeetCode Merge_k_Sorted_Lists

---
+++ src/main/java/com/codingTest/leetcode/hard/Merge_k_Sorted_Lists.java
@@ -0,0 +1,41 @@
+package com.codingTest.leetcode.hard;
+
+
+import java.util.PriorityQueue;
+
+
+public class Merge_k_Sorted_Lists {
+        public class ListNode {
+            int val;
+            ListNode next;
+            ListNode(int x) {
+                val = x;
+            }
+        }
+
+        public ListNode mergeKLists(ListNode[] lists) {
+            PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+            ListNode dummy = new ListNode(0);
+            ListNode current = dummy;
+
+            for (ListNode node : lists) {
+                if (node != null) {
+                    pq.add(node);
+                }
+            }
+
+            while (!pq.isEmpty()) {
+                ListNode minNode = pq.poll();
+                current.next = minNode;
+                current = current.next;
+
+                if (minNode.next != null) {
+                    pq.add(minNode.next);
+                }
+            }
+
+            return dummy.next;
+        }
+
+}

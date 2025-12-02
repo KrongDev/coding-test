e8865d1e4071c86a0946e442425ceef9a5528b0d
Leegeon <leegeondev@gmail.com>
2025-12-02 23:34:38 +0900

    feat: LeetCode Swap_Nodes_in_Pairs

---
+++ src/main/java/com/codingTest/leetcode/med/Swap_Nodes_in_Pairs.java
@@ -0,0 +1,38 @@
+package com.codingTest.leetcode.med;
+
+public class Swap_Nodes_in_Pairs {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode res = head.next;
+        ListNode prev = head, next;
+        prev.next = res.next;
+        res.next = prev;
+        for (ListNode node = prev.next; node != null; node = node.next) {
+            if (node.next == null) break;
+            next = node.next;
+            node.next = next.next;
+            next.next = node;
+            prev.next = next;
+            prev = node;
+        }
+        return res;
+    }
+}

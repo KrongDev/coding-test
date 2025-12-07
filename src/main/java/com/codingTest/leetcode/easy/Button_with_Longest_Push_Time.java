ec07bc9bd5bb9cb7203c7f15befe15a75ef7b487
Leegeon <leegeondev@gmail.com>
2025-12-07 23:21:30 +0900

    feat: LeetCode Button_with_Longest_Push_Time

---
+++ src/main/java/com/codingTest/leetcode/easy/Button_with_Longest_Push_Time.java
@@ -0,0 +1,16 @@
+package com.codingTest.leetcode.easy;
+
+public class Button_with_Longest_Push_Time {
+    public int buttonWithLongestTime(int[][] events) {
+        int res = events[0][0];
+        int resTime = events[0][1];
+        for(int i = 1; i < events.length; i++) {
+            int pressTime = events[i][1] - events[i - 1][1];
+            if(pressTime > resTime || (pressTime == resTime && events[i][0] < res)) {
+                res = events[i][0];
+                resTime = pressTime;
+            }
+        }
+        return res;
+    }
+}

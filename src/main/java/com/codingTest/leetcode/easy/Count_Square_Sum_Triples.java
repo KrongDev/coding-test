c36d70eb9211797ff3326d39ce191382c2606052
Leegeon <leegeondev@gmail.com>
2025-12-08 15:06:12 +0900

    feat: LeetCode Count_Square_Sum_Triples

---
+++ src/main/java/com/codingTest/leetcode/easy/Count_Square_Sum_Triples.java
@@ -0,0 +1,25 @@
+package com.codingTest.leetcode.easy;
+
+public class Count_Square_Sum_Triples {
+    public int countTriples(int n) {
+        int count = 0;
+
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                long sumOfSquares = (long)a * a + (long)b * b;
+                if (sumOfSquares > (long)n * n) {
+                    continue;
+                }
+
+                double sqrtValue = Math.sqrt(sumOfSquares);
+
+                long c = Math.round(sqrtValue);
+                if (c * c == sumOfSquares && c <= n) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}

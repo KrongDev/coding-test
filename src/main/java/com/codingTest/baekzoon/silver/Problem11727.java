0fe7446d558abb15e61c160ca40d6cf5cf656de3
Leegeon <leegeondev@gmail.com>
2026-01-08 12:57:56 +0900

    feat: 백준 실버3 11727 문제해결

---
+++ src/main/java/com/codingTest/baekzoon/silver/Problem11727.java
@@ -0,0 +1,27 @@
+package com.codingTest.baekzoon.silver;
+
+import java.io.*;
+
+public class Problem11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+
+        dp[1] = 1;
+        if(N >= 2) {
+            dp[2] = 3;
+        }
+
+        for(int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + (2* dp[i - 2]))%10007;
+        }
+
+        bw.write(dp[N] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
